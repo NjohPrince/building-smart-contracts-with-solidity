49db7db469c5132e52a3dcba87bfc6192adb508f
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-14 13:48:45 +0100

    example types of variables

---
+++ 002-variable-and-types.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+// variables are used to store information to be referenced
+// or manipulated by our program
+
+// Variable types:
+
+// Boolean - keyword: bool - Example values: true, false
+// Integer - keyword: uint - Example values: signed and unsigned integers with varying sizes
+// String - keyword: string - Example values: data values that are made up of a sequence of characters
+
+
--- variable-and-types.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >= 0.7.0 < 0.9.0;
-
-// variables are used to store information to be referenced
-// or manipulated by our program
-
