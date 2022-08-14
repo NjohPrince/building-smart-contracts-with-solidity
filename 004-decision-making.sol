83dde1f432e3ad2639ad1308d2f2b1cc480c959c
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-15 00:29:10 +0100

    if...else condition

--- 004-decision-making.sol
+++ 004-decision-making.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >= 0.7.0 < 0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * Learn about decision making in solidity
+ * conditional statements, switching execution paths
+ * based on certain conditions or decision made
  */
 
 contract learnDecisionMaking {
-    
+    // orange variable
+    uint256 oranges = 5;
+
+    // validate oranges function
+    function validateOranges() public view returns (bool) {
+        // if...else condition
+        if (oranges == 5) // evaluates trueness of the expression in parenthesis
+        {
+            // then
+            return true;
+        } else {
+            return true;
+        }
+    }
 }
