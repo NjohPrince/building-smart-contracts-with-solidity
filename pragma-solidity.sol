a63b214b6609f0542a66752b13bd67d1249268a0
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-14 13:24:22 +0100

    specifying solidity versions

---
+++ pragma-solidity.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// specify the solidity version you code is currently being written in
+// pragma is generally the first line in a solidity file and it is a 
+// directive that specifies the compiler version to be used for
+// the solidity file
+
+// single version
+// pragma solidity 0.8.0;
+
+// range version
+pragma solidity >= 0.7.0 < 0.9.0;
