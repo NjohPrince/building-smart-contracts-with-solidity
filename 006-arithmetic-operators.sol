ce76d021c0a7582a2f83546f9061065f664eb47c
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-15 16:21:47 +0100

    arithmetic operators

--- 006-arithmetic-operators.sol
+++ 006-arithmetic-operators.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * Learn about arithmetic operators in solidity
+ * operators tell the compiler to perform specific
+ * mathematical operation, logical or relational
+ * operation and produce a suitable response
  */
 
 contract learnArithmeticOperators {
