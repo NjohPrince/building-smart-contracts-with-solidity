b3a81b55e6af5e5be0507326a2a532cf50a18a9a
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-18 12:50:33 +0100

    fallback functions

---
+++ 023-fallback-functions.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Fallback functions
+ *
+ * Define a fallback function:
+ * 1. Cannot have a name
+ * 2. Does not take any inputs
+ * 3. Does not return any outputs
+ * 4. Must be declared as external
+ */
+
+ // USAGE - Why Use It
+ /**
+ * When you call a function that does not exists
+ * when transfering ethers; send and transfer receives 2300 gas
+ * bu call method receives more(all of the gas).
+ */
+
+contract LearnFallBackFunctions {
+    
+}
