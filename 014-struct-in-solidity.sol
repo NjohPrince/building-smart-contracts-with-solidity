26314f54979977903e0af29cfd624468ba3d65ab
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-18 07:58:58 +0100

    working with struct

---
+++ 014-struct-in-solidity.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Working with structs in solidity
+ *
+ * Structs are types that are used to represent a record.
+ * Supposed you want to keep track of movies in a library
+ * You might want to keep track of the following
+ * properties about each movie:
+ *
+ *
+ */
+
+contract LearnStructs {
+
+}
