a6834dfd0e485b1e755ca2286905b772df52ea74
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-18 10:33:10 +0100

    special variables

---
+++ 020-global-variables.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Global variables(special variables) in solidity
+ * These are variables that provide information about the blockchain
+ *
+ * As examples we have:
+ * msg.sender - sender of the message (current call),
+ * msg.value (uint) - number of wei sent with the message,
+ * block.timestamp - current block timestamp as seconds since unix epoch,
+ * block.number - current block number
+ */
+
+contract LearnGlobalVariables {
+    
+}
