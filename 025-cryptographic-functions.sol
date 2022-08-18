54da1d7cb9aa2c9fa50757b64deadc265dd2437d
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-18 15:04:35 +0100

    returning the resultsa

---
+++ 025-cryptographic-functions.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Cryptographic Functions
+ * A cryptographic has function(CHF) is a mathematical algorithm that maps data of arbitrary size
+ * (often called the "message") to a bit array of a fixed size (called the "hash" or "hash value" or "message digest")
+ * It is a one way function, that is, a function which is practically infeasible to invert or reverse the computation.
+ *
+ * Solidity provides inbuilt cryptographic functions as well. Below are some of the important methods it provides:
+ *
+ * keccak256(bytes memory) returns (byte32)
+ * sha256(bytes memory) returns (bytes 32)
+ * ripemd160(bytes memory) returns (bytes 20)
+ */
+
+// Keccak is a leading hashing function, designed by non-NSA designers. Keccak won NIST competition to become the official SHA3
+// Keccak is a family of cryptographic sponge functions and is designed as an alternative to SHA-256
+
+contract LearnCryptoGraphicFunctions {
+
+}
