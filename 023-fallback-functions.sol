e20904054eb32437e9acaf5f9f94574561188b00
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-18 14:04:45 +0100

    testing

--- 023-fallback-functions.sol
+++ 023-fallback-functions.sol
@@ -12,13 +12,44 @@ pragma solidity >=0.7.0 <0.9.0;
  * 4. Must be declared as external
  */
 
- // USAGE - Why Use It
- /**
+// USAGE - Why Use It
+/**
  * When you call a function that does not exists
  * when transfering ethers; send and transfer receives 2300 gas
  * bu call method receives more(all of the gas).
  */
 
 contract LearnFallBackFunctions {
-    
+    event Log(uint256 gas);
+
+    fallback() external payable {
+        // not recommended to write to much code - because the function will fail
+
+        // invoke the send or transfer method: we get 2300 gas which is enough to emit a log
+        // invoke the call method: we get all the gas
+
+        // special solidity function gasLeft returns how many gas is left
+        emit Log(gasleft());
+    }
+
+    function getBalance() public view returns (uint256) {
+        // return the stored balance of the contract
+        return address(this).balance;
+    }
+}
+
+// new contract will send ether to the FallBack contract
+contract SendToFallBack {
+    function transferToFallBack(address payable _to) public payable {
+        // send ether with transfer method
+        // automatically transfer will transfer 2300 gas amount
+
+        _to.transfer(msg.value);
+    }
+
+    function callFallBack(address payable _to) public payable {
+        // send ether with call
+        (bool sent, ) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send!");
+    }
 }
