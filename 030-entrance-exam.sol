8b430fe3e5cd2e11f8dc30450446dda0061c2494
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-20 10:55:51 +0100

    created three contracts C. D, E

---
+++ 030-entrance-exam.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Entrance exam into Advanced Solidity Concepts
+ */
+
+ // contract C
+contract C {
+    
+}
+
+// contract D
+contract D {
+    
+}
+
+
+// contract E
+contract E {
+    
+}
