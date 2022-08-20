ed14eac04e7bf081577857eb4fe0fac10014a30a
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-20 04:27:30 +0100

    restricted access -more on

---
+++ 029-restricted-access.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Restricted Access
+ * By Default a contract is made readonly unless it is made public
+ * We can restrict who can modify a contract's state or call a 
+ * function using modifiers.
+ */
+
+contract RestrictedAccess {
+    
+}
