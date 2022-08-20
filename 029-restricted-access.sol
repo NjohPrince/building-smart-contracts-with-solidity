f5e6f8d8e0956079c5c1eabe7043a7c6f9d18d58
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-20 10:33:06 +0100

    disown owner

--- 029-restricted-access.sol
+++ 029-restricted-access.sol
@@ -5,10 +5,29 @@ pragma solidity >=0.7.0 <0.9.0;
 /**
  * Restricted Access
  * By Default a contract is made readonly unless it is made public
- * We can restrict who can modify a contract's state or call a 
+ * We can restrict who can modify a contract's state or call a
  * function using modifiers.
  */
 
 contract RestrictedAccess {
-    
+    address public owner = msg.sender;
+    uint256 public creationTime = block.timestamp;
+
+    modifier onlyByOwner(address _account) {
+        // check if the current sender is the owner
+        // require(msg.sender == owner);
+        require(msg.sender == _account, "Access Restricted to Owners Only!");
+        _;
+    }
+
+    // function that changes the owner address
+    // restrict this access only to the owner of the contract
+    function changeOwnerAddress(address newAddres) public onlyByOwner(owner) {
+        owner = newAddres;
+    }
+
+    // funcion to disown the current owner
+    function disownOwner() public {
+        delete owner;
+    }
 }
