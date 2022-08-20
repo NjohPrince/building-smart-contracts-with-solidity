c042102c4fc4d14c13e602a99ab9c0f0cdd7dd68
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-20 20:33:55 +0100

    function with a brief example

--- 031-docstrings-in-solidity.sol
+++ 031-docstrings-in-solidity.sol
@@ -14,7 +14,7 @@ pragma solidity >=0.7.0 <0.9.0;
  * @author Njoh Noh Prince Junior => author's name
  * context of use: contracts, interfaces and functions
  *
- * @dev => exra details
+ * @dev => extra details
  * context of use: contracts, interfaces and functions
  */
 
@@ -29,5 +29,15 @@ contract DocStrings {
      */
     function someRandomFunction(address _addr) public pure returns (address) {
         return _addr;
+    }
+
+    address storedStateAddress;
+
+    /**
+     * @param _addr: address
+     * @dev changes the state variable storedStateAddress to a new one
+     */
+    function storeNewAddress(address _addr) public {
+        storedStateAddress = _addr;
     }
 }
