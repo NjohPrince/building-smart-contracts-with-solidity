0a344d93121ee198dfca70a38e15fd61bc542884
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-20 21:40:49 +0100

    review on functions

--- 024-function-overloading.sol
+++ 024-function-overloading.sol
@@ -8,6 +8,7 @@ pragma solidity >=0.7.0 <0.9.0;
  *
  * The function definition of the function must differ from each other by types and/or
  * the number of arguments in the argument list
+ * Also, the type of the arguments can be different.
  *
  * NOTE: You cannot overload function declarations that differ only by return type
  */
@@ -31,12 +32,33 @@ contract LearnFunctionOverLoading {
         return numberOne + numberTwo + numberThree;
     }
 
+    // 😅 Sum two strings --concatenation of two strings
+    function calculateSum(string memory first, string memory second)
+        public
+        returns (string memory)
+    {
+        // return the concatenation of both strings
+        return first + second;
+    }
+
+    // concatenate two names
+    function concatenateTwoString() public returns (string memory) {
+        string memory fullName = calculateSum("Njoh Noh", "Prince Junior");
+        return fullName;
+    }
+
+    // call the function to add two numbers and return the result
+    // of the computation.
     function calculateSumTwoArgs() public pure returns (uint256) {
+        // assign the result of the computation to a variable
         uint256 result = calculateSum(3, 4);
         return result;
     }
 
+    // call the function to add three numbers and return the result
+    // of the computation.
     function calculateSumThreeArgs() public pure returns (uint256) {
+        // assign the result of the computation to a variable
         uint256 result = calculateSum(3, 4, 1);
         return result;
     }
