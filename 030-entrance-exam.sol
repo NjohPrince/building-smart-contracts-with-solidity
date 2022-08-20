6339395847e36464690a0e68f623ee602eb23198
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-20 11:00:13 +0100

    created a private variable data in contract C

--- 030-entrance-exam.sol
+++ 030-entrance-exam.sol
@@ -4,20 +4,25 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * Entrance exam into Advanced Solidity Concepts
+ * @author Njoh Noh Prince Junior
  */
 
- // contract C
+// contract C
 contract C {
-    
+    uint256 private data;
 }
 
 // contract D
-contract D {
-    
+contract D is C {
+    function accessContentFromContractC() public view returns (uint256) {
+        // throws an error
+        // as variable data is not public
+        
+        // return data;
+    }
 }
-
 
 // contract E
 contract E {
-    
+
 }
