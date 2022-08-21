3587d653666058f3188d0360341685b291e8faf2
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 14:25:37 +0100

    upgraded

--- 037-libraries-in-solidity.sol
+++ 037-libraries-in-solidity.sol
@@ -22,6 +22,78 @@ pragma solidity >=0.7.0 <0.9.0;
  * 5. A Library cannot be inherited.
  */
 
-contract Library {
+library SearchLibrary {
+    function indexOf(uint256[] storage self, uint256 value)
+        public
+        view
+        returns (uint256)
+    {
+        for (uint256 i = 0; i < self.length; i++) {
+            if (self[i] == value) return i;
+        }
 
+        // not practical
+        // this is just so we do not get a warning first
+        return 0;
+    }
+}
+
+contract LibraryTest {
+    uint256[] data;
+
+    // instantiating our state
+    constructor() {
+        data.push(0);
+        data.push(1);
+        data.push(2);
+        data.push(3);
+        data.push(4);
+        data.push(5);
+    }
+
+    // check if given value is present within the array
+    function isValuePresent(uint256 val) external view returns (uint256) {
+        uint256 value = val;
+        uint256 index = SearchLibrary.indexOf(data, value);
+
+        return index;
+    }
+}
+
+// Exercise
+library SearchLibrary2 {
+    function indexOf(uint256[] storage self, uint256 value)
+        public
+        view
+        returns (uint256)
+    {
+        for (uint256 i = 0; i < self.length; i++) {
+            if (self[i] == value) return i;
+        }
+
+        // not practical
+        // this is just so we do not get a warning first
+        return 0;
+    }
+}
+
+contract LibraryTest2 {
+    using SearchLibrary2 for uint256[];
+    uint256[] data;
+
+    // instantiating our state
+    constructor() {
+        data.push(0);
+        data.push(1);
+        data.push(2);
+        data.push(3);
+        data.push(4);
+        data.push(5);
+    }
+
+    // check if given value is present within the array
+    function isValuePresent() external view returns (uint256) {
+        uint256 index = data.indexOf(4);
+        return index;
+    }
 }
