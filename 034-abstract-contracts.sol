b736cb920d0a37dfbdeeab5168cd94e5ab0d8054
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 11:59:16 +0100

    method overriding

--- 034-abstract-contracts.sol
+++ 034-abstract-contracts.sol
@@ -12,8 +12,22 @@ pragma solidity >=0.7.0 <0.9.0;
  *
  * Derived contract will implement the abstract function and use
  * the existing functions as they are and when required.
+ *
+ * Here we talk of two things
+ * 1. base contract and
+ * 2. the derived contract
  */
 
-contract LearnMoreOnEvents {
-    
+// this will act as out base contract
+abstract contract LearnMoreOnEvents {
+    // since the function has no implementation we can mark it as virtual
+    function sendMessage() public view virtual returns (string memory) {}
+}
+
+// derived contract
+contract DerivedContract is LearnMoreOnEvents {
+    // overriding the method within the base contract
+    function sendMessage() public pure override returns (string memory) {
+        return "Hello World!";
+    }
 }
