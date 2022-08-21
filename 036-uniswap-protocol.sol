fb4928dac30ce874cb364b4b463761b462b968ce
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 13:33:31 +0100

    got the Uniswap factory address and dai a stable coin on etherscan

--- 036-uniswap-protocol.sol
+++ 036-uniswap-protocol.sol
@@ -14,6 +14,9 @@ pragma solidity >=0.7.0 <0.9.0;
 // https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/UniswapV2Factory.solimplementation
 // UniswapV2Factory is deployed at 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f on the Ethereum mainnet, and the Ropsten, Rinkeby, Görli, and Kovan testnets
 
+// NOTE: Please do not deploy these contracts unless you knwo what you are doing
+// You will pay gas fees which youy might not want to
+
 interface UniswapV2Factory {
     // get the pair of tokens --tokenA and tokenB
     function getPair(address tokenA, address tokenB)
@@ -31,4 +34,10 @@ interface UniswapV2Pair {
             uint112 reserve1,
             uint32 blockTimestampLast
         );
+}
+
+contract MyContract {
+    address private factoryAddress = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address private dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    
 }
