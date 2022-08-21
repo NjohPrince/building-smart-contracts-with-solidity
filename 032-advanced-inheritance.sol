af81ba678be66f5233333458eb5a435b5cd38519
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 11:08:23 +0100

    notes about classes and contracts

---
+++ 032-advanced-inheritance.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Advanced Inheritance
+ *
+ * @title Advances Inheritance
+ * @notice Inheritance is a way to extend functionality of a contract
+ * Solidity supports single as well as multiple inheritance
+ *
+ * @dev A contract in solidity is similar to a class in C++ --which ia a blueprint for an object
+ * Classes can inherit other classes, so can contracts inherit other contracts!
+ *
+ * REMINDER: A constructor is a special function declared with the constructor keyword which will be
+ * executed once per contract and is invoked when a contract is created.
+ */
+
+// contract C
+contract AdvancedInheritance {
+    
+}
