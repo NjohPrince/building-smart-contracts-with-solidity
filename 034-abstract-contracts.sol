26d107e2a2faa26c9f9c2ae973379ef5902dfd77
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 11:48:08 +0100

    abstract contract contracts --intro

---
+++ 034-abstract-contracts.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Abstract Contracts
+ * @notice Abstract contracts is one which contains a least one
+ *         function without any implementation.
+ *
+ * Generally an abstract contract contains both implemented
+ * and abstract functions.
+ *
+ * Derived contract will implement the abstract function and use
+ * the existing functions as they are and when required.
+ */
+
+contract LearnMoreOnEvents {
+    
+}
