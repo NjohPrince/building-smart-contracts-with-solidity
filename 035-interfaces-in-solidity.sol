5357e57a8846bb8cafafa51cde756601977c4a5b
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 12:37:28 +0100

    incrementing while counting addresses

--- 035-interfaces-in-solidity.sol
+++ 035-interfaces-in-solidity.sol
@@ -15,6 +15,31 @@ pragma solidity >=0.7.0 <0.9.0;
  * using the interface name dot notation.
  */
 
-contract InterfacesInSolidity {
+contract Counter {
+    uint256 public count;
 
+    function increment() external {
+        count += 1;
+    }
+}
+
+// creating interface
+interface ICounter {
+    function count() external view returns (uint256);
+
+    function increment() external;
+}
+
+// to execute copy the address of the Counter contract
+// and feed into the parameters of the functions defined
+// at MyContract
+
+contract MyContract {
+    function incrementCounter(address _counter) external {
+        ICounter(_counter).increment();
+    }
+
+    function getCount(address _counter) external view returns (uint256) {
+        return ICounter(_counter).count();
+    }
 }
