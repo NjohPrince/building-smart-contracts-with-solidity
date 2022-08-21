234cae95ad1fbfd7de33a0c634a5bcd98992ff5b
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 12:15:09 +0100

    interfaces in solidity

---
+++ 035-interfaces-in-solidity.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Interfaces
+ * @notice Interfaces are similar to abstract contracts
+ *         and are created using interface keyword.
+ *
+ * Interfaces can not have any function with implementation
+ * Functions of an interface can be only of type external
+ * Interfaces can not have a constructor
+ * They can not have a state variable
+ * Interface can have enum, structs which can be accessed
+ * using the interface name dot notation.
+ */
+
+contract InterfacesInSolidity {
+
+}
