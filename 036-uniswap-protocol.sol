d9da9f81ab4984ca4ae3dc8af6d64d7a23ba8bab
theunicorndev237 <thefullstackunicorn237@gmail.com>
2022-08-21 13:26:50 +0100

    get reserves

--- 036-uniswap-protocol.sol
+++ 036-uniswap-protocol.sol
@@ -22,3 +22,13 @@ interface UniswapV2Factory {
         returns (address pair);
 }
 
+interface UniswapV2Pair {
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+}
